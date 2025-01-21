1c98f384d5230d4ffb006c6912afd6e4c66c7ab1
raditanurhasna <raditanurhasna1410@gmail.com>
2025-01-21 17:24:42 +0700

    Memperbaiki dan menambahkan halaman input lokasi

--- app/src/main/java/com/example/projectakhir/ui/navigation/PengelolaHalaman.kt
+++ app/src/main/java/com/example/projectakhir/ui/navigation/PengelolaHalaman.kt
@@ -7,9 +7,11 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.projectakhir.ui.screens.EntryAcaraScreen
-import com.example.projectakhir.ui.view.Acara.DestinasiHome
-import com.example.projectakhir.ui.view.Acara.HomeScreen
-import com.example.projectakhir.ui.view.Acara.DetailAcaraScreen
+import com.example.projectakhir.ui.view.Acara.*
+import com.example.projectakhir.ui.view.Lokasi.DestinasiEntryLokasi
+import com.example.projectakhir.ui.view.Lokasi.EntryLokasiScreen
+import com.example.projectakhir.ui.view.Lokasi.HomeScreenLokasi
+import com.example.restapi.ui.view.DetailView
 
 object DestinasiEntry {
     const val route = "entry"
@@ -17,6 +19,18 @@ object DestinasiEntry {
 
 object DestinasiDetail {
     const val route = "detail"
+}
+
+object DestinasiUpdate {
+    const val route = "update"
+}
+
+object DestinasiHomeLokasi {
+    const val route = "homeLokasi"
+}
+
+object DestinasiHome {
+    const val route = "home"
 }
 
 @Composable
@@ -29,31 +43,77 @@ fun PengelolaHalaman(
         startDestination = DestinasiHome.route,
         modifier = modifier,
     ) {
+        // Halaman utama
         composable(DestinasiHome.route) {
             HomeScreen(
                 navigateToItemEntry = { navController.navigate(DestinasiEntry.route) },
+                navigateToLokasi = { navController.navigate(DestinasiHomeLokasi.route) },
+                navigateToKlien = {},
+                navigateToVendor = {},
+                onLokasiClick = { navController.navigate(DestinasiHomeLokasi.route) },
+                onKlienClick = {},
+                onVendorClick = {},
                 onDetailClick = { itemId ->
-                    // Navigasi ke halaman detail dengan itemId
                     navController.navigate("${DestinasiDetail.route}/$itemId")
                 }
             )
         }
 
+        // Halaman input acara
         composable(DestinasiEntry.route) {
             EntryAcaraScreen(
                 navigateBack = { navController.popBackStack() }
             )
         }
 
-        // Menambahkan composable untuk DetailAcaraScreen
-        composable("${DestinasiDetail.route}/{itemId}") { backStackEntry ->
-            val itemId = backStackEntry.arguments?.getString("itemId")
-            if (itemId != null) {
-                DetailAcaraScreen(
-                    id = itemId,
-                    onBackClick = { navController.popBackStack() }
+        // Halaman input lokasi
+        composable(DestinasiEntryLokasi.route) {
+            EntryLokasiScreen(
+                navigateBack = { navController.popBackStack() }
+            )
+        }
+
+        // Halaman detail (item atau lokasi)
+        composable("${DestinasiDetail.route}/{id}") { backStackEntry ->
+            val id = backStackEntry.arguments?.getString("id")
+            if (id != null) {
+                DetailView(
+                    id = id,
+                    onNavigateBack = { navController.popBackStack() },
+                    onEditClick = {}
                 )
             }
         }
+
+
+
+        // Halaman edit/update acara
+        composable("${DestinasiUpdate.route}/{itemId}") { backStackEntry ->
+            val itemId = backStackEntry.arguments?.getString("itemId")
+            if (itemId != null) {
+                UpdateScreen(
+                    id = itemId,
+                    onNavigateBack = { navController.popBackStack() }
+                )
+            }
+        }
+
+        // Halaman lokasi
+        composable(DestinasiHomeLokasi.route) {
+            HomeScreenLokasi(
+                navigateToLokasiEntry = {
+                    navController.navigate(DestinasiEntry.route)
+                },
+                onDetailClick = { idLokasi ->
+                    navController.navigate("${DestinasiDetail.route}/$idLokasi")
+                },
+                onBack = {
+                    // Menggunakan popBackStack untuk kembali ke halaman sebelumnya
+                    navController.popBackStack()
+                }
+            )
+        }
+
+
     }
 }
