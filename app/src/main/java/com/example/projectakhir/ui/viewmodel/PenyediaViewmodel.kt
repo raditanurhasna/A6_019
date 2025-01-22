52411cde5aba674987062e8f89a81db9c607858f
raditanurhasna <raditanurhasna1410@gmail.com>
2025-01-22 09:12:43 +0700

    Menambahkan home, insert dan detail lokasi

--- app/src/main/java/com/example/projectakhir/ui/viewmodel/PenyediaViewmodel.kt
+++ app/src/main/java/com/example/projectakhir/ui/viewmodel/PenyediaViewmodel.kt
@@ -6,15 +6,23 @@ import androidx.lifecycle.viewmodel.CreationExtras
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
 import com.example.projectakhir.AcaraApplications
-import com.example.projectakhir.ui.viewmodel.acara.DetailAcaraViewModel
 import com.example.projectakhir.ui.viewmodel.acara.HomeViewModel
 import com.example.projectakhir.ui.viewmodel.acara.InsertViewModel
+import com.example.projectakhir.ui.viewmodel.acara.UpdateViewModel
+import com.example.projectakhir.ui.viewmodel.lokasi.DetailViewModel
+import com.example.projectakhir.ui.viewmodel.lokasi.HomeViewModelLokasi
+import com.example.projectakhir.ui.viewmodel.lokasi.InsertViewModelLokasi
+import com.example.restapi.ui.viewmodel.DetailAcaraViewModel
 
 object PenyediaViewModel {
     val Factory = viewModelFactory {
         initializer { HomeViewModel(aplikasiAcara().container.acaraRepository) }
         initializer { InsertViewModel(aplikasiAcara().container.acaraRepository) }
         initializer { DetailAcaraViewModel(aplikasiAcara().container.acaraRepository) }
+        initializer { UpdateViewModel(aplikasiAcara().container.acaraRepository) }
+        initializer { HomeViewModelLokasi(aplikasiAcara().container.lokasiRepository) }
+        initializer { InsertViewModelLokasi(aplikasiAcara().container.lokasiRepository) }
+        initializer { DetailViewModel(aplikasiAcara().container.lokasiRepository) }
 
     }
 }
