e6a5233e05bdb2957f4c42c53603832214a6c0b9
raditanurhasna <raditanurhasna1410@gmail.com>
2025-01-22 11:42:17 +0700

    Menambahkan insert lokasi pada view

---
+++ app/src/main/java/com/example/projectakhir/ui/view/Lokasi/InsertView.kt
@@ -0,0 +1,155 @@
+package com.example.projectakhir.ui.view.Lokasi
+
+import com.example.projectakhir.ui.customewidget.CostumeTopAppBar
+import com.example.projectakhir.ui.navigation.DestinasiNavigasi
+import com.example.projectakhir.ui.viewmodel.PenyediaViewModel
+import com.example.projectakhir.ui.viewmodel.lokasi.InsertUiEvent
+import com.example.projectakhir.ui.viewmodel.lokasi.InsertUiState
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
+import androidx.compose.material3.Divider
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.projectakhir.ui.viewmodel.lokasi.InsertViewModelLokasi
+
+import kotlinx.coroutines.launch
+
+object DestinasiEntryLokasi : DestinasiNavigasi {
+    override val route = "item_entry"
+    override val titleRes = "Daftar Lokasi"
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EntryLokasiScreen(
+    navigateBack: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: InsertViewModelLokasi = viewModel(factory = PenyediaViewModel.Factory)
+) {
+    val coroutineScope = rememberCoroutineScope()
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+    Scaffold(
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            CostumeTopAppBar(
+                title = DestinasiEntryLokasi.titleRes,
+                canNavigateBack = true,
+                scrollBehavior = scrollBehavior,
+                navigateUp = navigateBack
+            )
+        }
+    ) { innerPadding ->
+        EntryBody(
+            insertUiState = viewModel.uiState,
+            onLokasiValueChange = viewModel::updateInsertLokasiState,
+            onSaveClick = {
+                coroutineScope.launch {
+                    viewModel.insertLokasi()
+                    navigateBack()
+                }
+            },
+            modifier = Modifier
+                .padding(innerPadding)
+                .verticalScroll(rememberScrollState())
+                .fillMaxWidth()
+        )
+    }
+}
+
+@Composable
+fun EntryBody(
+    insertUiState: InsertUiState,
+    onLokasiValueChange: (InsertUiEvent) -> Unit,
+    onSaveClick: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Column(
+        verticalArrangement = Arrangement.spacedBy(18.dp),
+        modifier = modifier.padding(12.dp)
+    ) {
+        FormInput(
+            insertUiEvent = insertUiState.insertUiEvent,
+            onValueChange = onLokasiValueChange,
+            modifier = Modifier.fillMaxWidth()
+        )
+        Button(
+            onClick = onSaveClick,
+            shape = MaterialTheme.shapes.small,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Text(text = "Simpan")
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun FormInput(
+    insertUiEvent: InsertUiEvent,
+    modifier: Modifier = Modifier,
+    onValueChange: (InsertUiEvent) -> Unit = {},
+    enabled: Boolean = true
+) {
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.spacedBy(12.dp)
+    ) {
+        OutlinedTextField(
+            value = insertUiEvent.idLokasi,
+            onValueChange = { onValueChange(insertUiEvent.copy(idLokasi = it)) },
+            label = { Text("ID Lokasi") },
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+        OutlinedTextField(
+            value = insertUiEvent.namaLokasi,
+            onValueChange = { onValueChange(insertUiEvent.copy(namaLokasi = it)) },
+            label = { Text("Nama Lokasi") },
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+        OutlinedTextField(
+            value = insertUiEvent.alamatLokasi,
+            onValueChange = { onValueChange(insertUiEvent.copy(alamatLokasi = it)) },
+            label = { Text("Alamat") },
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+        OutlinedTextField(
+            value = insertUiEvent.kapasitas,
+            onValueChange = { onValueChange(insertUiEvent.copy(kapasitas = it)) },
+            label = { Text("Kapasitas") },
+            modifier = Modifier.fillMaxWidth(),
+            enabled = enabled,
+            singleLine = true
+        )
+        if (enabled) {
+            Text(
+                text = "Isi Semua Data!",
+                modifier = Modifier.padding(12.dp)
+            )
+        }
+        Divider(
+            thickness = 8.dp,
+            modifier = Modifier.padding(12.dp)
+        )
+    }
+}
