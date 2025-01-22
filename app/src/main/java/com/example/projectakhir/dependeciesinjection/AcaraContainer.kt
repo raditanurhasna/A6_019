98c411689d27dc294242919e525710848ac390cf
raditanurhasna <raditanurhasna1410@gmail.com>
2025-01-22 11:34:02 +0700

    Menambahkan lokasi dan klien pada acara container

--- app/src/main/java/com/example/projectakhir/dependeciesinjection/AcaraContainer.kt
+++ app/src/main/java/com/example/projectakhir/dependeciesinjection/AcaraContainer.kt
@@ -1,8 +1,14 @@
 package com.example.projectakhir.dependeciesinjection
 
 import com.example.projectakhir.repository.AcaraRepository
+import com.example.projectakhir.repository.KlienRepository
+import com.example.projectakhir.repository.LokasiRepository
 import com.example.projectakhir.repository.NetworkAcaraRepository
+import com.example.projectakhir.repository.NetworkKlienRepository
+import com.example.projectakhir.repository.NetworkLokasiRepository
 import com.example.projectakhir.service_api.AcaraService
+import com.example.projectakhir.service_api.KlienService
+import com.example.projectakhir.service_api.LokasiService
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import kotlinx.serialization.json.Json
 import okhttp3.MediaType.Companion.toMediaType
@@ -10,9 +16,11 @@ import retrofit2.Retrofit
 
 interface AppContainer {
     val acaraRepository: AcaraRepository
+    val lokasiRepository: LokasiRepository
+    val klienRepository: KlienRepository
 }
 
-class AcaraContainer : AppContainer {
+class AppContainerImpl : AppContainer {
 
     private val baseUrl = "http://10.0.2.2:80/projectakhir/"
     private val json = Json { ignoreUnknownKeys = true }
@@ -21,12 +29,32 @@ class AcaraContainer : AppContainer {
         .baseUrl(baseUrl)
         .build()
 
+    // Acara Service
     private val acaraService: AcaraService by lazy {
         retrofit.create(AcaraService::class.java)
     }
 
+    // Lokasi Service
+    private val lokasiService: LokasiService by lazy {
+        retrofit.create(LokasiService::class.java)
+    }
+
+    // Klien Service
+    private val klienService: KlienService by lazy {
+        retrofit.create(KlienService::class.java)
+    }
+
+    // Repositories
     override val acaraRepository: AcaraRepository by lazy {
         NetworkAcaraRepository(acaraService)
     }
-}
 
+
+    override val lokasiRepository: LokasiRepository by lazy {
+        NetworkLokasiRepository(lokasiService)
+    }
+
+    override val klienRepository: KlienRepository by lazy {
+        NetworkKlienRepository(klienService)
+    }
+}
